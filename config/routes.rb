453cdd6ce18268beb243c808d948ee67ee964b80
pauline jouve <pauline.jouve@edhec.com>
2020-02-13 15:00:30 +0100

    add cocktail part

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # A user can see the list of cocktails
+  # A user can create a new cocktail
+  resources :cocktails, only: [:create, :index, :show, :new] do
+  # A user can see the details of a given cocktail, with the dose needed for each ingredient
+   resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy, :show, :index]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Creating Ingredients...'
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint = Ingredient.create!(name: "mint leaves")
+
+puts 'Creating Coctails...'
+mojito = Cocktail.create!(name: "Mojito")
+capi = Cocktail.create!(name: "Capi")
+
+puts 'Creating Doses...'
+dose1 = Dose.create!(description: "5ml", ingredient: lemon, cocktail: mojito)
+dose2 = Dose.create!(description: "piler la glace avant", ingredient: ice, cocktail: mojito)
+
+puts 'Finished!'
