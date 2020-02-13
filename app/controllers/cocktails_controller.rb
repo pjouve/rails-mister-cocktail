b9874cfbb724242e7a11a9e31244f3228d324def
pauline jouve <pauline.jouve@edhec.com>
2020-02-13 17:33:12 +0100

    rake ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,6 +7,7 @@ class CocktailsController < ApplicationController
   def show
   @cocktail = Cocktail.find(params[:id])
   @doses = @cocktail.doses
+
   end
 
   def new
@@ -16,7 +17,7 @@ class CocktailsController < ApplicationController
   def create
   @cocktail = Cocktail.new(cocktail_params)
   if @cocktail.save
-    redirect_to cocktails_path
+    redirect_to cocktail_path(@cocktail)
   else
     render :new
   end
