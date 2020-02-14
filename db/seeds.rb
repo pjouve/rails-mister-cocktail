55f54ce771fd151774c2c9bc8b0f15fc4391ca52
pauline jouve <pauline.jouve@edhec.com>
2020-02-14 16:48:58 +0100

    add some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -16,11 +17,17 @@ ice = Ingredient.create!(name: "ice")
 mint = Ingredient.create!(name: "mint leaves")
 
 puts 'Creating Coctails...'
-mojito = Cocktail.create!(name: "Mojito")
-capi = Cocktail.create!(name: "Capi")
+file_mojito = URI.open('https://assets.afcdn.com/recipe/20180705/80255_w648h344c1cx2774cy1849.jpg')
+mojito = Cocktail.new(name: "Mojito")
+
+mojito.photo.attach(io: file_mojito, filename: 'mojito.jpg', content_type: 'image/jpg')
+mojito.save
+
+#capi = Cocktail.create!(name: "Capi")
 
 puts 'Creating Doses...'
 dose1 = Dose.create!(description: "5ml", ingredient: lemon, cocktail: mojito)
 dose2 = Dose.create!(description: "piler la glace avant", ingredient: ice, cocktail: mojito)
 
 puts 'Finished!'
+
